e4bb25e8f389987bab98f864ec7e800797fb4519
Tomcat88 <thomas.introini@audiencerate.com>
2022-09-28 14:10:38 +0200

    :sparkles: add webhook

--- pg_capup.py
+++ pg_capup.py
@@ -12,6 +12,11 @@ current_time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 
 print(f"Backing up databases at {current_time}.")
 
+def http_notification_helper():
+  if (config["notifications"]["http_webhook"] != ""):
+    requests.get(
+      config["notifications"]["http_webhook"]
+    )
 def slack_notification_helper(content):
   if (config["notifications"]["slack_webhook"] != ""):
     requests.post(
@@ -26,12 +31,12 @@ def upload_helper(database, location):
   if (location["type"] == "S3"):
     s3 = boto3.resource(
       's3',
-      endpoint_url = location["connection"]["endpoint_url"],
       aws_access_key_id = location["connection"]["aws_access_key_id"],
       aws_secret_access_key = location["connection"]["aws_secret_access_key"]
     )
     s3.Bucket(location["connection"]["bucket"]).upload_file(f'{config["working_path"]}{database["name"]}.sql', f"{database['name']}-{current_time}.sql")
     slack_notification_helper(f"Uploaded {database['name']} to {location['name']}.")
+    http_notification_helper()
   else:
     return
 
